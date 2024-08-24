2f694d57816ccff88ce9abd4a35abef09abfce94
Håvard Anda Estensen <haavard.ae@gmail.com>
2024-08-24 11:11:18 +0200

    Implement temp conversions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    let celsius = 100.0;
+    let fahrenheit = celsius_to_fahrenheit(celsius);
+    let kelvin = celsius_to_kelvin(celsius);
+
+    println!("{}°C is {}°F", celsius, fahrenheit);
+    println!("{}°C is {}°K", celsius, kelvin);
+}
+
+fn celsius_to_fahrenheit(celsius: f64) -> f64 {
+    (celsius * 9.0 / 5.0) + 32.0
+}
+
+fn celsius_to_kelvin(celsius: f64) -> f64 {
+    celsius + 273.15
+}
+
+fn fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
+    (fahrenheit - 32.0) * 5.0 / 9.0
+}
+
+fn fahrenheit_to_kelvin(fahrenheit: f64) -> f64 {
+    celsius_to_kelvin(fahrenheit_to_celsius(fahrenheit))
+}
+
+fn kelvin_to_celsius(kelvin: f64) -> f64 {
+    kelvin - 273.15
+}
+
+fn kelvin_to_fahrenheit(kelvin: f64) -> f64 {
+    celsius_to_fahrenheit(kelvin_to_celsius(kelvin))
 }
